5a2f26dffaba44606f479d47965909266ef19a9e
dvarubla <dvarubla@users.noreply.github.com>
2017-05-06 21:26:20 +0700

    PdfLaTeX support

--- build.bat
+++ build.bat
@@ -1,1 +1,18 @@
-cmd /V /C "mkdir build && cd build && latexmkmod -r ../.latexmkmodrc ../tex/rpz.tex %*"
+@echo off
+set RESTVAR=
+set LASTVAR=
+:loop1
+if "%1"=="" goto after_loop
+	set RESTVAR=%RESTVAR% %LASTVAR%
+	set LASTVAR=%1
+	shift
+goto loop1
+
+:after_loop
+
+if "%LASTVAR%"=="" (
+	echo Specify build dir!
+	exit /b 1
+)
+if not exist "%LASTVAR%" mkdir %LASTVAR%
+cmd /V /C "cd %LASTVAR% && latexmkmod -r ../.latexmkmodrc ../tex/rpz.tex %RESTVAR%"
--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-mkdir -p build && cd build && ../utils/latexmkmod -r ../.latexmkmodrc ../tex/rpz.tex $@
+#!/usr/bin/env sh
+
+all_args=( "$@" )
+if [ "${#all_args[@]}" -eq "0" ]; then
+	echo "Specify build dir!"
+	exit 1
+else
+	build_dir=all_args[${#all_but_last[@]}-1]
+	unset "all_args[${#all_but_last[@]}-1]"
+fi
+mkdir -p $build_dir && cd $build_dir && \
+../utils/latexmkmod -r ../.latexmkmodrc ../tex/rpz.tex ${all_args[*]}
