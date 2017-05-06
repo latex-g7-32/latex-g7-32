f82875c43679d816dc9b6be1df6671c478a82c9c
dvarubla <dvarubla@users.noreply.github.com>
2017-05-06 22:40:44 +0700

    build.sh fix

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ if [ "${#all_args[@]}" -eq "0" ]; then
 	echo "Specify build dir!"
 	exit 1
 else
-	build_dir=all_args[${#all_but_last[@]}-1]
-	unset "all_args[${#all_but_last[@]}-1]"
+	build_dir="${all_args[${#all_args[@]}-1]}"
+	unset "all_args[${#all_args[@]}-1]"
 fi
+
 mkdir -p $build_dir && cd $build_dir && \
 ../utils/latexmkmod -r ../.latexmkmodrc ../tex/rpz.tex ${all_args[*]}
